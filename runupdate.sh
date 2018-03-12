c626bac04e8a69d7fe7ba971133876da27e838e3
Gang Li <gangli@tulip.org.au>
2018-03-13 00:12:18 +1100

    Create runupdate.sh

---
+++ runupdate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+cat ./brewlist/homebrew-packages.txt > list.tmp
+brew leaves >> list.tmp
+sort list.tmp|uniq > ./brewlist/homebrew-packages.txt
+
+cat ./brewlist/homebrew-cask.txt > list.tmp
+brew cask list >> list.tmp
+sort list.tmp|uniq > ./brewlist/homebrew-cask.txt
+
+rm *.tmp
+ 
+
