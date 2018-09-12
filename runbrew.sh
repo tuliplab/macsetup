ad9a548610168b1cc999f6dcca2afa55f52df319
Gang Li <gangli@gmail.com>
2018-09-12 15:49:21 +1000

    pdf

--- runbrew.sh
+++ runbrew.sh
@@ -28,6 +28,7 @@ brew tap caskroom/drivers
 brew tap caskroom/fonts 
 brew tap buo/cask-upgrade
 brew tap Homebrew/bundle
+brew tap vizjerai/extras
 
 cat ./brewlist/homebrew-packages.txt|xargs brew install
 cat ./brewlist/homebrew-cask.txt|xargs brew cask install
@@ -56,3 +57,13 @@ npm i hexo-renderer-markdown-it --save
 # Install Mac Clean up https://github.com/fwartner/mac-cleanup
 sh -c "$(wget https://raw.githubusercontent.com/fwartner/mac-cleanup/master/installer.sh -O -)"
 
+# Install PDF size optmizer
+# https://github.com/pts/pdfsizeopt
+mkdir ~/Cloud/Dropbox/Apps/bin/pdfsizeopt
+cd ~/Cloud/Dropbox/Apps/bin/pdfsizeopt
+curl -L -o pdfsizeopt_libexec_darwin.tar.gz https://github.com/pts/pdfsizeopt/releases/download/2017-09-03d/pdfsizeopt_libexec_darwin-v1.tar.gz
+tar xzvf pdfsizeopt_libexec_darwin.tar.gz
+rm -f    pdfsizeopt_libexec_darwin.tar.gz
+curl -L -o pdfsizeopt.single https://raw.githubusercontent.com/pts/pdfsizeopt/master/pdfsizeopt.single
+chmod +x pdfsizeopt.single
+ln -s pdfsizeopt.single pdfsizeopt
